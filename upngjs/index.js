2930d8a071e4777cfd7bbf510457e868166e3b84
EndRollModel <51425629+EndRollModel@users.noreply.github.com>
2020-11-24 17:55:04 +0800

    feat : 製作apng的方法

---
+++ upngjs/index.js
@@ -0,0 +1,19 @@
+const uPng = require('upng-js');
+const fs = require('fs');
+const delay = 250; // 延遲時間
+const oneBuffer = fs.readFileSync('./input/test01.png');
+const twoBuffer = fs.readFileSync('./input/test02.png');
+const threeBuffer = fs.readFileSync('./input/test03.png');
+const bufferArray = []; // 所有圖片fs放這裡
+const encodeArray = []; // 解析之後的buffer放這裡
+const delayArray = []; // 除非需要手動改延遲 不然固定延遲
+bufferArray.push(oneBuffer, twoBuffer, threeBuffer);
+bufferArray.forEach((e) => {
+    const decode = uPng.decode(e); // 解析
+    const rbgImg = uPng.toRGBA8(decode); // 取得Rgba8內容
+    encodeArray.push(rbgImg[0]); // 取得Buffer
+    delayArray.push(delay); // 寫入延遲
+});
+const encAction = uPng.encode(encodeArray, 300, 300, 0, delayArray); // 製作apngBuffer
+fs.writeFileSync('./output/uPng.png', new Uint8Array(encAction)); // 輸出
+
---
+++ upngjs/readme.md
@@ -0,0 +1,7 @@
+##### 使用套件 ： [upng-js](https://www.npmjs.com/package/upng-js)    
+
+內容 ： 將 png 製作成 apng 實作方法
+
+```npm install upng-js ```
+
+
